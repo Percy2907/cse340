27ba0d00d2817e097a4e8b9871aa45f13a79845e
Percy2907 <pyarlequejara@byupathway.edu>
2025-11-01 16:46:35 -0500

    W01 Assignment: Site Creation

--- server.js
+++ server.js
@@ -10,20 +10,32 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+
 /* ***********************
  * View Engine and Templates
  *************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
+
 // Index route
 app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
+  // Definimos messages como función para evitar el error
+  const messages = () => {
+    return '<p>Bienvenido a CSE Motors!</p>'
+  }
+
+  res.render("index", {
+    title: "Home",
+    messages: messages
+  })
 })
+
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
