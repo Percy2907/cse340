8f4182ef6cb2879950380727da15fc3dd4126075
Percy2907 <pyarlequejara@byupathway.edu>
2025-11-12 15:46:40 -0500

    Content Delivery and the MVC Pattern

--- server.js
+++ server.js
@@ -10,11 +10,16 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require("./utilities/")
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+
+
 
 /* ***********************
  * View Engine and Templates
@@ -24,17 +29,35 @@ app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
 
 // Index route
-app.get("/", function(req, res){
-  // Definimos messages como función para evitar el error
-  const messages = () => {
-    return '<p>Bienvenido a CSE Motors!</p>'
-  }
+app.get("/", utilities.handleErrors(baseController.buildHome))
 
-  res.render("index", {
-    title: "Home",
-    messages: messages
+// Inventory routes
+app.use("/inv", inventoryRoute)
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if (err.status==404) {
+    message = err.message
+  } else {
+    message = "Server is not available."
+  }
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
   })
 })
+
 
 /* ***********************
  * Local Server Information
