287ec2e514f2fad74c9528b9688000265dcc3283
Percy2907 <pyarlequejara@byupathway.edu>
2025-11-27 13:36:52 -0500

    JWT Authorization

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -14,6 +14,7 @@ router.get("/register", utilities.handleErrors(accountController.buildRegister))
 // Default account management view
 router.get(
   "/",
+  utilities.checkLogin,
   utilities.handleErrors(accountController.buildAccountManagement)
 );
 
